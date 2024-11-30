776b40960924f5b19f0974f874de5c195289b315
MiguelLuz <migoluz@gmail.com>
2024-11-30 00:29:10 -0300

    alterei o codigo para corresponder ao meu estudo

--- ecmascript/ecmascript.js
+++ ecmascript/ecmascript.js
@@ -1,68 +1,13 @@
 "use strict";
-// string
-let nome = 'João';
-console.log(nome);
-// nome =28
-//numbers
-let idade = 27;
-console.log(idade);
-//boolean
-let possuiHobbies = false;
-console.log(possuiHobbies);
-//tipos explícitos
-let minhaIdade;
-minhaIdade = 27;
-console.log(typeof minhaIdade);
-//array
-let hobbies = ["Cozinhar", "Praticar Esportes"];
-console.log(hobbies[0]);
-console.log(typeof hobbies);
-hobbies = [100, 200, 300];
-console.log(hobbies);
-//Tuplas
-let endereco = ["Av Principal", 99, 'matematica'];
-console.log(endereco);
-endereco = ['Rua Boa', 1260, 'precioso'];
-console.log(endereco);
-//enums
-var Cor;
-(function (Cor) {
-    Cor[Cor["Cinza"] = 0] = "Cinza";
-    Cor[Cor["Verde"] = 100] = "Verde";
-    Cor[Cor["Azul"] = 10] = "Azul";
-    Cor[Cor["Laranja"] = 11] = "Laranja";
-})(Cor || (Cor = {}));
-let minhaCor = Cor.Verde;
-console.log(minhaCor);
-console.log(Cor.Azul);
-console.log(Cor.Laranja);
-//any
-let carro = 'BMW';
-console.log(carro);
-carro = { marca: 'BMW', ano: 2019 };
-console.log(carro);
-//funções
-function retornaMeuNome() {
-    return nome;
+// let & const
+let seraQuePode = "?";
+console.log(seraQuePode);
+let estaFrio = true;
+if (estaFrio) {
+    let acao = 'Colocar o casaco!';
+    console.log(acao);
 }
-console.log(retornaMeuNome());
-function digaOi() {
-    console.log('oi');
-    // return minhaIdade
-}
-digaOi();
-function multiplicar(numA, numB) {
-    return numA * numB;
-}
-console.log(multiplicar(2, 9));
-//tipos função
-let calculo;
-calculo = multiplicar;
-console.log(calculo(5, 6));
-//objetos
-let usuario = {
-    nome: "João",
-    idade: 27
-};
-console.log(usuario);
+const cpf = '123.456.678-99';
+//cpf = '382197389-324' não consigo fqazer isso porque a const, é o valor que é unico
+console.log(cpf);
 //# sourceMappingURL=ecmascript.js.map
